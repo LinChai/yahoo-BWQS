f3a7b6abc4f020e1fb2a7647cb8aee509c8a3c3c
LinChai <linchai.grad@gmail.com>
2017-03-16 13:57:00 -0700

    Update generateSD.cc

--- src/generateSD.cc
+++ src/generateSD.cc
@@ -15,7 +15,11 @@ int main() {
   for(int i = 0; i < cache_size.size(); i++) {
     if(i == 0) {
       for(int S = 10; S < 20000; S+=10) {
-        int D = (cache_size[i] - 4*F - L *S *(12+L/8))/(4*F+L*S/8);
+        int D = 0;
+        if(L%8 == 0)
+          D = (cache_size[i] - 4*F - L *S *(12+L/8))/(4*F+S*L/8);
+        else
+          D = (cache_size[i] - 4*F - L *S *(12+L/8+1))/(4*F+S*(L/8+1));
         if(D <= 0)
           break;
         cout << "(" << cache_size[i] << ", " << D << ", " << S <<")" << endl;;
@@ -24,7 +28,11 @@ int main() {
     else if(i == 1) {
       cout << "#########" << endl;
       for(int S = 100; S < 20000; S +=100) {
-        int D = (cache_size[i] - 4*F - L *S *(12+L/8))/(4*F+L*S/8);
+        int D = 0;
+        if(L%8 == 0)
+          D = (cache_size[i] - 4*F - L *S *(12+L/8))/(4*F+S*L/8);
+        else
+          D = (cache_size[i] - 4*F - L *S *(12+L/8+1))/(4*F+S*(L/8+1));
         if(D <= 0)
           break;
         cout << "(" << cache_size[i] << ", " << D << ", " << S <<")"<< endl;
@@ -33,7 +41,11 @@ int main() {
     else {
       cout << "########" << endl;
       for(int S = 500; S < 20000; S +=500) {
-        int D = (cache_size[i] - 4*F - L *S *(12+L/8))/(4*F+L*S/8);
+        int D = 0;
+        if(L%8 == 0)
+          D = (cache_size[i] - 4*F - L *S *(12+L/8))/(4*F+S*L/8);
+        else
+          D = (cache_size[i] - 4*F - L *S *(12+L/8+1))/(4*F+S*(L/8+1));
         if(D <= 0)
           break;
         cout << "(" << cache_size[i] << ", " << D << ", " << S <<")" << endl;
