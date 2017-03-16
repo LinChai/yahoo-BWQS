31805676acd78e2de21f62e10db396885edf0c99
LinChai <linchai.grad@gmail.com>
2017-03-15 22:30:03 -0700

    Update batch_exe.sh

--- batch_exe.sh
+++ batch_exe.sh
@@ -21,11 +21,38 @@ do
         sed -i "s/#define S.*/#define S 3000 \/\/ s/g" src/BWQS8bit_full_optm_yahoo_8_9.c
         sed -i "s/#define D.*/#define D 134 \/\/ d/g" src/BWQS8bit_full_optm_yahoo_8_10.c
         sed -i "s/#define S.*/#define S 5000 \/\/ s/g" src/BWQS8bit_full_optm_yahoo_8_10.c
+        
         LEAF=8
+        make
+        echo "result of Leaf_num=$LEAF"
+        for VAR in 1 2 3 4 5 6 7 8 9 10
+        do
+            echo 'BWQS8bit_full_optm_yahoo_8_'$VAR
+            target/BWQS8bit_full_optm_yahoo_8_$VAR -ensemble ./fast_rank/yahoo/tree-ensemble-file_20000_$LEAF -instances ./fast_rank/yahoo/100k.txt -maxLeaves $LEAF
+        done    
     fi
     if ["$LOOP" = "2"]; then
-        sed -i "s/#define D.*/#define D 1 \/\/ d/g" src/BWQS16bit_full_optm_yahoo1.c
-        sed -i "s/#define S.*/#define S 100 \/\/ s/g" src/BWQS16bit_full_optm_yahoo1.c
+        sed -i "s/#define D.*/#define D 1 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_1.c
+        sed -i "s/#define S.*/#define S 75 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_1.c
+        sed -i "s/#define D.*/#define D 2 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_2.c
+        sed -i "s/#define S.*/#define S 50 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_2.c
+        sed -i "s/#define D.*/#define D 31 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_3.c
+        sed -i "s/#define S.*/#define S 200 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_3.c
+        sed -i "s/#define D.*/#define D 3 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_4.c
+        sed -i "s/#define S.*/#define S 800 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_4.c
+        sed -i "s/#define D.*/#define D 15 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_5.c
+        sed -i "s/#define S.*/#define S 500 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_5.c
+        sed -i "s/#define D.*/#define D 297 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_6.c
+        sed -i "s/#define S.*/#define S 1000 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_6.c
+        sed -i "s/#define D.*/#define D 234 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_7.c
+        sed -i "s/#define S.*/#define S 1500 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_7.c
+        sed -i "s/#define D.*/#define D 189 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_8.c
+        sed -i "s/#define S.*/#define S 2000 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_8.c
+        sed -i "s/#define D.*/#define D 169 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_9.c
+        sed -i "s/#define S.*/#define S 3000 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_9.c
+        sed -i "s/#define D.*/#define D 67 \/\/ d/g" src/BWQS16bit_full_optm_yahoo_10_10.c
+        sed -i "s/#define S.*/#define S 5000 \/\/ s/g" src/BWQS16bit_full_optm_yahoo_10_10.c
+      
         LEAF=10
     fi
     if ["$LOOP" = "3"]; then
@@ -43,8 +70,7 @@ do
     if ["$LOOP" = "7"]; then
         LEAF=150
     fi
-    make
-    echo "result of Leaf_num=$LEAF"
+   
     for VAR in 1 2 3 4 5 6 7 8 9 10
     do
         echo 'BWQS8bit_full_optm_yahoo_8_'$VAR
