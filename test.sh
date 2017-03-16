87d2d3673fabb6e27cbdf1dd25d1811448c4239a
LinChai <linchai.grad@gmail.com>
2017-03-16 14:10:59 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ do
     while IFS=' ' read -r cache_size D S
     do
         cp "src/BWQS"$LEAF"bit_full_optm_yahoo.c" "src/BWQS"$LEAF"bit_full_optm_yahoo_$D_$S.c"
-        #sed -i "s/#define D.*/#define D $D \/\/ d/g" src/BWQS$LEAFbit_full_optm_yahoo_$D_$S.c
-        #sed -i "s/#define S.*/#define S $S \/\/ s/g" src/BWQS$LEAFbit_full_optm_yahoo_$D_$S.c
+        sed -i "s/#define D.*/#define D "$D" \/\/ d/g" "src/BWQS"$LEAF"bit_full_optm_yahoo_"$D"_"$S".c"
+        sed -i "s/#define S.*/#define S "$S" \/\/ s/g" "src/BWQS"$LEAF"bit_full_optm_yahoo_"$D"_"$S".c"
     done < "$input"
 done
